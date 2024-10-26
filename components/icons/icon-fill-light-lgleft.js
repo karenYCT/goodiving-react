198a4f77bacdae3ae2f2f7c1120362959a4505f8
Karen <jaka08131129@gmail.com>
2024-10-27 01:05:08 +0800

    新增共用元件icon的功能

--- components/icons/icon-fill-light-lgleft.js
+++ components/icons/icon-fill-light-lgleft.js
@@ -1,10 +1,14 @@
-import styles from './icons.module.css'
-import { FaChevronLeft } from 'react-icons/fa'
+import styles from './icons.module.css';
+import { FaChevronLeft } from 'react-icons/fa';
 
-export default function Button() {
+export default function Button({ classname = '', onClick = () => {} }) {
   return (
-    <button className={`${styles['color-fill-light']} ${styles['size-lg']} `}>
+    <button
+      className={`${styles['color-fill-light']} ${styles['size-lg']} `}
+      onClick={onClick}
+      aria-label="Previous"
+    >
       <FaChevronLeft />
     </button>
-  )
+  );
 }
--- components/icons/icon-fill-primary-smleft.js
+++ components/icons/icon-fill-primary-smleft.js
@@ -1,10 +1,14 @@
-import styles from './icons.module.css'
-import { FaChevronLeft } from 'react-icons/fa'
+import styles from './icons.module.css';
+import { FaChevronLeft } from 'react-icons/fa';
 
-export default function Button() {
+export default function Button({ classname = '', onClick = () => {} }) {
   return (
-    <button className={`${styles['color-fill-primary']} ${styles['size-sm']} `}>
+    <button
+      className={`${styles['color-fill-primary']} ${styles['size-sm']} `}
+      onClick={onClick}
+      aria-label="Previous"
+    >
       <FaChevronLeft />
     </button>
-  )
+  );
 }
