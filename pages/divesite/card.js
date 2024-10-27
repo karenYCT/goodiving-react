a66e61b501ae1a78ef6a34cc43ee246d712e1915
Karen <jaka08131129@gmail.com>
2024-10-27 22:58:02 +0800

    新增景點介紹卡片的RWD

---
+++ pages/divesite/card.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import Imgintrocard from '@/components/karen/siteintrocard';
+export default function Card(onClick) {
+  return (
+    <>
+      <div>
+        <Imgintrocard />
+      </div>
+    </>
+  )
+}
