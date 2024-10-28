56ab5c112b34d61f08b82ceb972f2116010c542c
tzu <tzutzu1021@gmail.com>
2024-10-28 18:38:40 +0800

    update home-page.js

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import styles from '../styles/home.module.css';
 import Navbar from '@/components/layouts/navbar';
+import Footer from '@/components/layouts/footer2';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Navbar />
+      <div className={styles.homeImgContainer}>
       <img className={styles.homeImg} src="/home-page.jpg" alt="" />
+      </div>
       <h1>home</h1>
-      <div style={{ height: '100vh' }}></div>
+      {/* <div style={{ height: '100vh' }}></div> */}
+      <Footer />
     </div>
   );
 }
