a5729f45537a4607ef22232fd3b1b58c9ad6d8d1
tzu <tzutzu1021@gmail.com>
2024-10-30 17:27:00 +0800

    add lessonpage

--- pages/lesson/[pid].js
+++ pages/lesson/[pid].js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '@/components/layouts/layout';
-import LeftSide from '@/components/layouts/leftSide';
+import RightSide from '@/components/layouts/rightSide';
 import layoutStyles from '@/components/layouts/layout.module.css';
-import styles from './index.module.css';
+import styles from './[pid].module.css';
 
 export default function Lesson(props) {
   return (
     <>
       <Layout>
-        <LeftSide>
-          <h1>Lesson</h1>
-        </LeftSide>
         <div className={layoutStyles.main}>
           <h1>Lesson</h1>
           <h1>Lesson</h1>
         </div>
+
+        <RightSide>
+          <h1>Lesson</h1>
+        </RightSide>
       </Layout>
     </>
   );
