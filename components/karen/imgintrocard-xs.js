00cb92698931914aeca7826b202afe534390cf48
Karen <jaka08131129@gmail.com>
2024-10-31 00:47:04 +0800

    更新imgintrocard元件

--- components/karen/imgintrocard-xs.js
+++ components/karen/imgintrocard-xs.js
@@ -4,19 +4,19 @@ import MiniTagGlass from '../tag/minitag-glass';
 import { FaShareAlt } from 'react-icons/fa';
 import { FaRegBookmark } from 'react-icons/fa6';
 
-export default function Imgintrocard({ 
-  location_english="", 
-  site_name="" 
+export default function Imgintrocard({
+  location_english = '',
+  site_name = '',
 }) {
   return (
     <div className={`${styles['imgContainer']}`}>
-        <div className={`${styles['textContainer']}`}>
-          <div className={`${styles['locationContainer']}`}>
-            <div className={`${styles['solid-line']}`}></div>
-            <p>{location_english}NORTHEAST COAST</p>
-          </div>
-          <h6>{site_name}鋼鐵礁</h6>
+      <div className={`${styles['textContainer']}`}>
+        <div className={`${styles['locationContainer']}`}>
+          <div className={`${styles['solid-line']}`}></div>
+          <p>{location_english}NORTHEAST COAST</p>
         </div>
+        <h6>{site_name}鋼鐵礁</h6>
+      </div>
       <img src="/siteimg.JPG" alt="" />
     </div>
   );
