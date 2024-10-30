46b16e5461cbbbd109e0e485c55c68d543afd37d
Shirley <shirley109000@gmail.com>
2024-10-30 16:30:38 +0800

    更新縣市選單的value(數字改成值)

--- pages/member/modify.js
+++ pages/member/modify.js
@@ -21,32 +21,34 @@ export default function Modify() {
 
   const tabItemss = ['更新個人資訊', '更新密碼'];
 
+  // 縣市資料
   const cityOptions = [
-    { value: '0', label: '請選擇縣市' },
-    { value: '1', label: '臺北市' },
-    { value: '2', label: '新北市' },
-    { value: '3', label: '桃園市' },
-    { value: '4', label: '臺中市' },
-    { value: '5', label: '臺南市' },
-    { value: '6', label: '高雄市' },
-    { value: '7', label: '宜蘭縣' },
-    { value: '8', label: '新竹縣' },
-    { value: '9', label: '苗栗縣' },
-    { value: '10', label: '彰化縣' },
-    { value: '11', label: '南投縣' },
-    { value: '12', label: '雲林縣' },
-    { value: '13', label: '嘉義縣' },
-    { value: '14', label: '屏東縣' },
-    { value: '15', label: '花蓮縣' },
-    { value: '16', label: '臺東縣' },
-    { value: '17', label: '澎湖縣' },
-    { value: '18', label: '基隆市' },
-    { value: '19', label: '嘉義市' },
-    { value: '20', label: '新竹市' },
-    { value: '21', label: '金門縣' },
-    { value: '22', label: '連江縣' },
+    { value: '請選擇縣市', label: '請選擇縣市' },
+    { value: '臺北市', label: '臺北市' },
+    { value: '新北市', label: '新北市' },
+    { value: '桃園市', label: '桃園市' },
+    { value: '臺中市', label: '臺中市' },
+    { value: '臺南市', label: '臺南市' },
+    { value: '高雄市', label: '高雄市' },
+    { value: '宜蘭縣', label: '宜蘭縣' },
+    { value: '新竹縣', label: '新竹縣' },
+    { value: '苗栗縣', label: '苗栗縣' },
+    { value: '彰化縣', label: '彰化縣' },
+    { value: '南投縣', label: '南投縣' },
+    { value: '雲林縣', label: '雲林縣' },
+    { value: '嘉義縣', label: '嘉義縣' },
+    { value: '屏東縣', label: '屏東縣' },
+    { value: '花蓮縣', label: '花蓮縣' },
+    { value: '臺東縣', label: '臺東縣' },
+    { value: '澎湖縣', label: '澎湖縣' },
+    { value: '基隆市', label: '基隆市' },
+    { value: '嘉義市', label: '嘉義市' },
+    { value: '新竹市', label: '新竹市' },
+    { value: '金門縣', label: '金門縣' },
+    { value: '連江縣', label: '連江縣' },
   ];
 
+  // 行秤區資料
   const arrayDistrict = [
     [],
     [
@@ -433,7 +435,7 @@ export default function Modify() {
   const [selectedDistrict, setSelectedDistrict] = useState(''); // 區域選擇
 
   const handleCityChange = (e) => {
-    setSelectedCity(e.target.value);
+    setSelectedCity(e.target.selectedIndex);
     setSelectedDistrict(''); // 重置行政區選擇
   };
 
@@ -490,7 +492,7 @@ export default function Modify() {
                           className={`${stylesModify.selectButton} ${
                             selectedCity ? stylesModify.selected : ''
                           }`}
-                          value={selectedCity}
+                          value={cityOptions[selectedCity]?.value}
                           onChange={handleCityChange}
                         >
                           {cityOptions.map((city) => (
@@ -508,7 +510,7 @@ export default function Modify() {
                           }`}
                           value={selectedDistrict}
                           onChange={handleDistrictChange}
-                          disabled={!selectedCity}
+                          disabled={selectedCity === 0}
                         >
                           <option value="">請選擇行政區</option>
                           {selectedCity &&
