6e68a759672c5103a68b6249f066ff7651a42b9e
tzu <tzutzu1021@gmail.com>
2024-11-01 00:15:02 +0800

    Update lesson/index.js

--- pages/lesson/index.js
+++ pages/lesson/index.js
@@ -3,15 +3,23 @@ import Layout from '@/components/layouts/layout';
 import LeftSide from '@/components/layouts/leftSide';
 import layoutStyles from '@/components/layouts/layout.module.css';
 import styles from './index.module.css';
+import Card2 from '@/components/eden/card2';
+import CheckoutFlow from '@/components/eden/checkout-flow';
 
-export default function Lesson(props) {
+export default function Lesson() {
   return (
     <>
+      <div></div>
       <Layout>
         <LeftSide>
-          <h1>Lesson</h1>
+          <div className={styles.layout}>
+            <h4>排序</h4>
+            <h4>篩選</h4>
+          </div>
         </LeftSide>
         <div className={layoutStyles.main}>
+          <Card2 />
+          <CheckoutFlow />
           <h1>Lesson</h1>
           <h1>Lesson</h1>
         </div>
