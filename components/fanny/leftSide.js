b2105c4ed0dc4f6b3852f949711dc9fd1d228126
Karen <jaka08131129@gmail.com>
2024-11-15 18:49:39 +0800

    fanny narbar 修正

--- components/fanny/leftSide.js
+++ components/fanny/leftSide.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import styles from '@/components/fanny/layout.module.css';
 
-export default function LeftSide({children}) {
+export default function LeftSide({ children }) {
   return (
     <>
-        <div className={styles['left-side']}>
-             {children}{/* 在這裡渲染左側的子內容 */}
-        </div>
-    
-
-
+      <div style={{ height: 'fit-content' }} className={styles['left-side']}>
+        {children}
+        {/* 在這裡渲染左側的子內容 */}
+      </div>
     </>
-  )
+  );
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
 import Layout from '@/components/fanny/layout';
 import LeftSide from '@/components/fanny/leftSide';
-import Navbar from '@/components/layouts/navbar';
+// import Navbar from '@/components/layouts/navbar';
 import Card from '@/components/fanny/card';
 import Search1lg from '@/components/fanny/search-1-lg';
 import Pagination from '@/components/fanny/pagination';
@@ -21,25 +21,28 @@ export default function Blog() {
   const tabItems = ['最新', '人氣'];
   const [activeCategory, setActiveCategory] = useState();
 
-  const fetchPosts = useCallback(async (keyword) => {
-    try {
-      setLoading(true);
-      let url = `${process.env.NEXT_PUBLIC_API_PATH}/api/blog`;
-      if (keyword && keyword.trim() !== '') {
-        url += `?keyword=${encodeURIComponent(keyword)}`;
+  const fetchPosts = useCallback(
+    async (keyword) => {
+      try {
+        setLoading(true);
+        let url = `${process.env.NEXT_PUBLIC_API_PATH}/api/blog`;
+        if (keyword && keyword.trim() !== '') {
+          url += `?keyword=${encodeURIComponent(keyword)}`;
+        }
+        const response = await fetch(url, {
+          headers: getAuthHeader ? await getAuthHeader() : {},
+        });
+        if (!response.ok) throw new Error('獲取文章資料失敗');
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('獲取文章資料錯誤:', error);
+      } finally {
+        setLoading(false);
       }
-      const response = await fetch(url, {
-        headers: getAuthHeader ? await getAuthHeader() : {},
-      });
-      if (!response.ok) throw new Error('獲取文章資料失敗');
-      const data = await response.json();
-      setPosts(data);
-    } catch (error) {
-      console.error('獲取文章資料錯誤:', error);
-    } finally {
-      setLoading(false);
-    }
-  }, [getAuthHeader]);
+    },
+    [getAuthHeader]
+  );
 
   const fetchCategories = useCallback(async () => {
     try {
@@ -60,10 +63,12 @@ export default function Blog() {
   }, [fetchPosts, fetchCategories]);
 
   useEffect(() => {
-    if (posts.length > 0) { // 確保 posts 有資料
-      const filtered = activeCategory === categories?.[0]?.id
-        ? posts
-        : posts.filter((post) => post.bc_id === activeCategory); // 修改為 bc_id
+    if (posts.length > 0) {
+      // 確保 posts 有資料
+      const filtered =
+        activeCategory === categories?.[0]?.id
+          ? posts
+          : posts.filter((post) => post.bc_id === activeCategory); // 修改為 bc_id
       setFilteredPosts(filtered);
     } else {
       setFilteredPosts([]); // 如果沒有文章，設為空陣列
@@ -76,77 +81,81 @@ export default function Blog() {
 
   return (
     <>
-      <Navbar />
-      <Search1lg search={handleSearch} />
+      {/* <Navbar /> */}
 
       <Layout>
-        <LeftSide>
-          <div className={styles.categoryContainer}>
-            {categories?.length > 0 &&
-              categories?.map((category) => (
-                <button
-                  key={category.id}
-                  onClick={() => setActiveCategory(category.id)}
-                  onKeyDown={(e) =>
-                    e.key === 'Enter' && setActiveCategory(category.id)
-                  }
-                  className={styles.categoryButton}
-                  aria-label={`選擇${category.name}分類`}
-                  type="button"
-                  style={{
-                    color: activeCategory === category.id ? 'white' : 'gray',
-                    cursor: 'pointer',
-                    padding: '10px',
-                    fontWeight:
-                      activeCategory === category.id ? 'bold' : 'normal',
-                    backgroundColor:
-                      activeCategory === category.id
-                        ? '#023e8a'
-                        : 'transparent',
-                    border: 'none',
-                    width: '100%',
-                    textAlign: 'left',
-                    borderRadius: '5px',
-                    marginBottom: '5px',
-                  }}
-                >
-                  {category.name}
-                </button>
-              ))}
-          </div>
-          <br />
-          <Link href="/blog/postmodal">
-            <Button>新增文章</Button>
-          </Link>
-        </LeftSide>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Search1lg search={handleSearch} />
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+            <LeftSide>
+              <div className={styles.categoryContainer}>
+                {categories?.length > 0 &&
+                  categories?.map((category) => (
+                    <button
+                      key={category.id}
+                      onClick={() => setActiveCategory(category.id)}
+                      onKeyDown={(e) =>
+                        e.key === 'Enter' && setActiveCategory(category.id)
+                      }
+                      className={styles.categoryButton}
+                      aria-label={`選擇${category.name}分類`}
+                      type="button"
+                      style={{
+                        color:
+                          activeCategory === category.id ? 'white' : 'gray',
+                        cursor: 'pointer',
+                        padding: '10px',
+                        fontWeight:
+                          activeCategory === category.id ? 'bold' : 'normal',
+                        backgroundColor:
+                          activeCategory === category.id
+                            ? '#023e8a'
+                            : 'transparent',
+                        border: 'none',
+                        width: '100%',
+                        textAlign: 'left',
+                        borderRadius: '5px',
+                        marginBottom: '5px',
+                      }}
+                    >
+                      {category.name}
+                    </button>
+                  ))}
+              </div>
+              <br />
+              <Link href="/blog/postmodal">
+                <Button>新增文章</Button>
+              </Link>
+            </LeftSide>
 
-        <div className={styles.main}>
-          {loading ? (
-            <div className="text-center py-4">載入中...</div>
-          ) : filteredPosts && filteredPosts.length > 0 ? (
-            filteredPosts.map((post) => (
-              <button
-                onClick={() => router.push(`/blog/${post.id}`)}
-                key={post.id}
-                className={styles.cardButton}
-                style={{
-                  cursor: 'pointer',
-                  width: '100%',
-                  padding: 0,
-                  border: 'none',
-                  background: 'none',
-                  textAlign: 'left',
-                }}
-              >
-                <Card post={post} />
-              </button>
-            ))
-          ) : (
-            <div className="text-center py-4">沒有找到相關文章</div>
-          )}
-          
-          <Pagination />
-          
+            <div className={styles.main}>
+              {loading ? (
+                <div className="text-center py-4">載入中...</div>
+              ) : filteredPosts && filteredPosts.length > 0 ? (
+                filteredPosts.map((post) => (
+                  <button
+                    onClick={() => router.push(`/blog/${post.id}`)}
+                    key={post.id}
+                    className={styles.cardButton}
+                    style={{
+                      cursor: 'pointer',
+                      width: '100%',
+                      padding: 0,
+                      border: 'none',
+                      background: 'none',
+                      textAlign: 'left',
+                    }}
+                  >
+                    <Card post={post} />
+                  </button>
+                ))
+              ) : (
+                <div className="text-center py-4">沒有找到相關文章</div>
+              )}
+
+              <Pagination />
+            </div>
+          </div>
         </div>
       </Layout>
     </>
