8161983ec8725299200f31efac1a0f0eb74e2ca4
Karen <jaka08131129@gmail.com>
2024-11-15 18:49:39 +0800

    fanny narbar 修正

--- components/fanny/layout.js
+++ components/fanny/layout.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import styles from './layout.module.css';
+import LoginModal from '@/components/shirley/loginModal';
+import { useAuth } from '@/context/auth-context';
+import Navbar from '@/components/layouts/navbar';
 
 export default function Layout({ children }) {
+  const { openModal, isOpen, closeModal } = useAuth();
+
   return (
-    <div className={styles['container']}>
-      {children} {/* 在這裡渲染 Layout 的子內容 */}
-    </div>
+    <>
+      <Navbar openModal={openModal} />
+      <LoginModal isOpen={isOpen} closeModal={closeModal} />
+      <div className={styles['container']}>
+        {children} {/* 在這裡渲染 Layout 的子內容 */}
+      </div>
+    </>
   );
 }
