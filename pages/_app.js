f617fa15ddc8ab1bc372436bb7ba123a1237ed08
tzu <tzutzu1021@gmail.com>
2024-11-20 19:18:39 +0800

    Update Head

--- pages/_app.js
+++ pages/_app.js
@@ -3,47 +3,55 @@ import '@/styles/globals.css';
 import { AuthContextProvider } from '@/context/auth-context';
 import { UserContextProvider } from '@/context/user-context';
 import { Toaster } from 'react-hot-toast';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <AuthContextProvider>
-      <UserContextProvider>
-        {getLayout(<Component {...pageProps} />)}
-        <Toaster
-          toastOptions={{
-            success: {
-              style: {
-                border: '2px solid #023e8a',
-                padding: '16px',
-                color: '#023e8a',
-                backgroundColor: '#fff',
+    <>
+      <Head>
+        <link rel="icon" href="/favicon-new.ico" />
+        <title>goodiving</title>
+      </Head>
+
+      <AuthContextProvider>
+        <UserContextProvider>
+          {getLayout(<Component {...pageProps} />)}
+          <Toaster
+            toastOptions={{
+              success: {
+                style: {
+                  border: '2px solid #023e8a',
+                  padding: '16px',
+                  color: '#023e8a',
+                  backgroundColor: '#fff',
+                },
+                iconTheme: {
+                  primary: '#023e8a',
+                  secondary: '#fff',
+                },
               },
-              iconTheme: {
-                primary: '#023e8a',
-                secondary: '#fff',
+              error: {
+                style: {
+                  border: '2px solid #023e8a',
+                  padding: '16px',
+                  color: '#023e8a',
+                  backgroundColor: '#fff',
+                },
+                iconTheme: {
+                  primary: '#ff277e',
+                  secondary: '#fff',
+                },
               },
-            },
-            error: {
-              style: {
-                border: '2px solid #023e8a',
-                padding: '16px',
-                color: '#023e8a',
-                backgroundColor: '#fff',
-              },
-              iconTheme: {
-                primary: '#ff277e',
-                secondary: '#fff',
-              },
-            },
-          }}
-          containerStyle={{
-            top: '120px',
-            zIndex: '10001',
-          }}
-        />
-      </UserContextProvider>
-    </AuthContextProvider>
+            }}
+            containerStyle={{
+              top: '120px',
+              zIndex: '10001',
+            }}
+          />
+        </UserContextProvider>
+      </AuthContextProvider>
+    </>
   );
 }
